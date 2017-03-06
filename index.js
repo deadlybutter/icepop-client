6ef007c2bf98882416f1050362d10c61be19dc2b
deadlybutter <thedeadlybutter@gmail.com>
2017-03-05 22:01:26 -0500

    Cleanup cleanup

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-import 'whatwg-fetch';
-
 class Client {
 
   /**
--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  loaders: [
-    {
-      test: /\.js$/,
-      exclude: /(node_modules|bower_components)/,
-      loader: 'babel-loader',
-    }
-  ]
-}
